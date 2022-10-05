6ef3b46cd06cc62f61af0c80af8403ef92fa397d
Daniel Boll <danielboll.academico@gmail.com>
2022-10-05 13:55:39 -0300

    chore(repo): move the source code as well
    Signed-off-by: Daniel Boll <danielboll.academico@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Daniel Boll</h1>
+      <span>Welcome to Daniel Boll's personal space</span>
+
+      <div className="socials">
+        <a href="https://github.com/Daniel-Boll">Github</a>
+        <a href="https://linkedin.com/in/Daniel-Boll">LinkedIn</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
