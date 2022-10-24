b82cdce87108e90ac5f8b2720b35dc934c7c88a6
Daniel Boll <danielboll.academico@gmail.com>
2022-10-24 20:17:57 -0300

    feat(nginx): add nginx conf
    Signed-off-by: Daniel Boll <danielboll.academico@gmail.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scp -i ~/.ssh/aws-key ./root/* ec2-user@ec2-3-16-109-171.us-east-2.compute.amazonaws.com:/var/www/daniel-boll.me/
+scp -r -i ~/.ssh/aws-key ./root/* ec2-user@ec2-3-16-109-171.us-east-2.compute.amazonaws.com:/var/www/daniel-boll.me/
